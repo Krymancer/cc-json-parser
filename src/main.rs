d36120dd1199b67ad98ba206dfa988b61b894143
Júnior Nascimento <junior.nascm@gmail.com>
2024-06-06 00:50:40 -0300

    feat: reimaging the parser

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,43 @@
-use std::{env, fs::File, io::{BufRead, BufReader}, process::exit};
+use std::{env, fs, io, process::exit};
+
+#[derive(Debug)]
+enum JsonValue {
+    Object(Vec<(String, JsonValue)>),
+    Array(Vec<JsonValue>),
+    String(String),
+    Number(f64),
+    Bool(bool),
+    Null,
+}
+
+#[derive(Debug)]
+enum Token {
+    CurlyOpen,
+    CurlyClose,
+    SquareOpen,
+    SquareClose,
+    Colon,
+    Comma,
+    String(String),
+    Number(f64),
+    Bool(bool),
+    Null,
+}
+
+// TODO: Reading a file to memory is not great
+// If the file is too big this cloud crash or be really slow
+// Try a stream or something later!!
+fn read_file(path: String) -> Result<String, std::io::Error> {
+    fs::read_to_string(path)
+}
+
+fn tokenize(input: String) -> Vec<Token> {
+    todo!("Implement tokenization")
+}
+
+fn parse(tokens: &[Token]) -> Result<JsonValue, String> {
+    todo!("Implement parsing")
+}
 
 fn main() {
     let mut args = env::args().skip(1);
@@ -12,54 +51,14 @@ fn main() {
 
     let file_path = file_path.unwrap();
 
-    let file = File::open(&file_path);
-
-    if let Err(err) = file {
-        eprintln!("error: opening file {}: {}", file_path, err);
-        exit(1);
-    }
-
-    let file = file.unwrap();
-
-    if file.metadata().unwrap().len() == 0 {
-        eprintln!("Empty file!");
-        exit(1);
-    }
-
-    let reader = BufReader::new(file);
-
-    let mut braces_count = 0;
-    let mut sq_braces_count = 0;
-
-    for line in reader.lines() {
-        let line = line.expect("Error reading line");
-        let line = line.trim();
-        
-        braces_count += line.chars().filter(|&c| c == '{').count();
-        braces_count -= line.chars().filter(|&c| c == '}').count();
-        sq_braces_count += line.chars().filter(|&c| c == '[').count();
-        sq_braces_count -= line.chars().filter(|&c| c == ']').count();
-
-        let mut pair = line.split(':');
-
-        let key = pair.next();
-
-        if let Some(key) = key {
-            let cases = vec!["{", "}", "[", "]"];
-            if !cases.contains(&key) {
-                if !key.starts_with('"') {
-                    eprintln!("expecting STRING found: {key}");
-                    exit(1);
-                }
-
-                println!("this is a valid key: {key}");
+    match read_file(file_path) {
+        Ok(content) => {
+            let tokens = tokenize(content);
+            match parse(&tokens) {
+                Ok(json_value) => println!("{:#?}", json_value),
+                Err(e) => eprintln!("Fail to parse JSON: {}", e),
             }
-        }    
-    }    
-
-    if braces_count != 0 || sq_braces_count != 0 {
-        eprintln!("Json incomplete or malformed!");
-        exit(1); 
-    }
-        
+        },
+        Err(e) => eprintln!("Failed to read file: {}", e),
+    }        
 }
